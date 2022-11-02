bcbd93c1a7ca01a33746b59abd1957bf81bf7d6b
Md Somrat Akbor <somrat876@gmail.com>
2022-11-02 23:49:53 +0600

    controller test case start

---
+++ CustomerInfo.Test/V1/Controllers/CustomerControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerInfo.Test.V1.Controllers
+{
+    public class CustomerControllerTest
+    {
+        public CustomerControllerTest()
+        {
+
+        }
+    }
+}
